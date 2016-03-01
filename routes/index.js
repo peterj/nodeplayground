30f4385d3d731535535e3321602c40a3cccc20af
Peter Jausovec <peterj@microsoft.com>
2016-03-01 08:28:49 -0800

    Adding platform

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var os = require('os');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', host: req.headers.host });
+  res.render('index', { title: 'Express', host: req.headers.host, platform: os.platform() });
 });
 
 module.exports = router;
